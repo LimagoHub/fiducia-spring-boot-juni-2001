da38528a39f5416c0483f7dec727a3d2b10400c7
Jo Wagner <jowagner@limago.de>
2021-06-10 16:18:38 +0200

    Response Entity

--- firstspringboot/src/main/java/de/fiducia/langermann/langer_mann/controllers/DemoController.java
+++ firstspringboot/src/main/java/de/fiducia/langermann/langer_mann/controllers/DemoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 //@Component
 //    @Service
 //    @Repository
@@ -21,5 +23,10 @@ public class DemoController {
     @GetMapping("gruss")
     public String getString() {
         return "Hallo Rest";
+    }
+
+    @GetMapping("uuid")
+    public String getUUID() {
+        return UUID.randomUUID().toString();
     }
 }
--- firstspringboot/src/main/java/de/fiducia/langermann/langer_mann/controllers/PersonController.java
+++ firstspringboot/src/main/java/de/fiducia/langermann/langer_mann/controllers/PersonController.java
@@ -2,11 +2,16 @@ package de.fiducia.langermann.langer_mann.controllers;
 
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.glassfish.jersey.uri.UriComponent;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/persons")
@@ -24,6 +29,8 @@ public class PersonController {
         ) );
     }
 
+
+    // Zweite Möglichkeit parameter zu übergeben (als Pfadvariable)
     @GetMapping(path="/{id}", produces = {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE} )
@@ -35,4 +42,47 @@ public class PersonController {
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok(PersonDTO.builder().id(id).vorname("John").nachname("Doe").build());
     }
+
+    @DeleteMapping (path="/{id}")
+    @ApiResponse(responseCode = "200", description = "Person wurde gelöscht")
+    @ApiResponse(responseCode = "404", description = "Person wurde nicht gefunden")
+    public ResponseEntity<Void> deleteById(@PathVariable String id){
+        if("4711".equals(id))
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok().build();
+    }
+
+//    // Dritte Möglichkeit parameter zu übergeben (als JSON)
+//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+//    @ApiResponse(responseCode = "200", description = "Person wurde geändert")
+//    @ApiResponse(responseCode = "201", description = "Person wurde gespeicher")
+//    public ResponseEntity<Void> saveOrUpdate(@RequestBody  PersonDTO dto, UriComponentsBuilder uriComponentsBuilder) {
+//        dto.setId(UUID.randomUUID().toString());
+//
+//        // Immer wenn kein Idempotetes Post
+//        UriComponents uriComponents = uriComponentsBuilder.path("v1/persons/{id}").buildAndExpand(dto.getId());
+//
+//        System.out.println("DTO: " + dto + " wurde gespeichert");
+//        return  ResponseEntity.created(uriComponents.toUri()).build();
+//    }
+
+    // Dritte Möglichkeit parameter zu übergeben (als JSON)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponse(responseCode = "200", description = "Person wurde geändert")
+    @ApiResponse(responseCode = "201", description = "Person wurde gespeicher")
+    public ResponseEntity<Void> saveOrUpdate(@RequestBody  PersonDTO dto) {
+
+
+        System.out.println("DTO: " + dto + " wurde gespeichert");
+        return  ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // Ersatz
+    @PostMapping(value = "/to-upper",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonDTO> toUpper(@RequestBody PersonDTO person) {
+        person.setNachname(person.getNachname().toUpperCase());
+        person.setVorname(person.getVorname().toUpperCase());
+        return ResponseEntity.ok(person);
+    }
+
 }
