4e36cab01e874351d925672953c16a3e2b050982
Jo Wagner <jowagner@limago.de>
2021-06-10 14:01:29 +0200

    Initial commit

---
+++ firstspringboot/src/main/java/de/fiducia/langermann/langer_mann/controllers/DemoController.java
@@ -0,0 +1,25 @@
+package de.fiducia.langermann.langer_mann.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Component
+//    @Service
+//    @Repository
+//    @Controller
+//        @RestController
+
+@RestController
+@RequestMapping("/v1/demo")
+public class DemoController {
+
+    @GetMapping("gruss")
+    public String getString() {
+        return "Hallo Rest";
+    }
+}
---
+++ firstspringboot/src/test/java/de/fiducia/langermann/langer_mann/FirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fiducia.langermann.langer_mann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
